d07940971ed8a7e80611c3ea575f03cf6b34c6c6
Dmytro <64677316+l-dmitry94@users.noreply.github.com>
2023-09-10 20:10:35 +0300

    update files

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,38 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Change code below this line
 
-console.log(galleryItems);
+//Створення галереї
+const galleryList = document.querySelector(".gallery");
+let arrGalleryItems = [];
+
+galleryItems.forEach(galleryItem => {
+    const item = `
+        <li class="gallery__item">
+            <a class="gallery__link" href="${galleryItem.original}">
+                <img
+                    class="gallery__image"
+                    src="${galleryItem.preview}"
+                    data-source="${galleryItem.original}"
+                    alt="${galleryItem.description}"
+                />
+            </a>
+        </li>
+    `;
+    arrGalleryItems.push(item);
+})
+const newArrGalleryItems = arrGalleryItems.join("");
+galleryList.insertAdjacentHTML("beforeend", newArrGalleryItems);
+//Створення галереї
+
+
+//Використання бібліотеки створення модального вікна із зображеннями
+let galleryItem = new SimpleLightbox('.gallery__link', {
+    captions: true,
+    captionsType: 'alt',
+    captionDelay: 250,
+    captionPosition: 'bottom'
+});
+//Використання бібліотеки створення модального вікна із зображеннями
