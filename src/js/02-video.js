3fb28033efec0657c39763274fed860f0d5a9a76
Dmytro <lyubadmitry2@gmail.com>
2023-09-10 22:04:24 +0300

    finished 2 tasks

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,25 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.getElementById('vimeo-player');
+const player = new Player(iframe);
+
+const LOCALSTORAGE_KEY = "videoplayer-current-time";
+
+function saveTime(time) {
+    localStorage.setItem(LOCALSTORAGE_KEY, time);
+}
+
+function restoreTime() {
+    const currentTime = localStorage.getItem(LOCALSTORAGE_KEY);
+    if (currentTime !== null) {
+        player.setCurrentTime(parseFloat(currentTime));
+    }
+}
+
+player.on('timeupdate', throttle(data => {
+    const currentTime = data.seconds;
+    saveTime(currentTime);
+}, 1000));
+
+restoreTime();
